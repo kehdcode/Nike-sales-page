2a0a4cdb53050ebfcd16bceb305fe9a9bd19f0c6
kehdcode <kehindedawood@gmail.com>
2019-10-21 10:02:12 -0700

    style, image, and js implemented

---
+++ main.js
@@ -0,0 +1,15 @@
+function shout(x) {
+    console.log('Hello world');
+    x.style.transform = "scale(1.5)";
+    document.getElementById("myImage").src ="Images/gh.jpg"
+}
+function normal(x) {
+    console.log('Hello world');
+    x.style.transform = "scale(1)";
+    document.getElementById("myImage").src ="Images/shoe.png"
+}
+function move(){
+ const y = document.getElementById = "myImage";
+ console.log("hello");
+    y.style.transform = "rotate(20deg)";
+}
