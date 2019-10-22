428a3f965e0418aba0c6ce1d78afc819c5a22642
kehdcode <kehindedawood@gmail.com>
2019-10-22 10:15:44 -0700

    over effect completed

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
-function shout(x) {
+
+
+function blueIn(x) {
     console.log('Hello world');
     x.style.transform = "scale(1.5)";
-    document.getElementById("myImage").src ="Images/gh.jpg"
+    document.getElementById("myImage").src ="Images/shoe.png"
 }
-function normal(x) {
+function blueOut(x) {
     console.log('Hello world');
     x.style.transform = "scale(1)";
     document.getElementById("myImage").src ="Images/shoe.png"
 }
-function move(){
- const y = document.getElementById = "myImage";
- console.log("hello");
-    y.style.transform = "rotate(20deg)";
+
+function redIn(x) {
+    var y = document.getElementsByClassName('hero');
+    console.log('Hello world');
+    x.style.transform = "scale(1.5)";
+    document.getElementById("myImage").src ="Images/Redshoe.png"
+    y.style.backgroundGradient = "red";
+}
+function redOut(x) {
+    console.log('Hello world');
+    x.style.transform = "scale(1)";
+    document.getElementById("myImage").src ="Images/shoe.png"
+}
+
+function purpleIn(x) {
+    console.log('Hello world');
+    x.style.transform = "scale(1.5)";
+    document.getElementById("myImage").src ="Images/purpleshoe.png"
+}
+function purpleOut(x) {
+    console.log('Hello world');
+    x.style.transform = "scale(1)";
+    document.getElementById("myImage").src ="Images/shoe.png"
+}
+
+function greenIn(x) {
+    console.log('Hello world');
+    x.style.transform = "scale(1.5)";
+    document.getElementById("myImage").src ="Images/greenshoe.png"
+    
+}
+function greenOut(x) {
+    console.log('Hello world');
+    x.style.transform = "scale(1)";
+    document.getElementById("myImage").src ="Images/greenshoe.png"
 }
