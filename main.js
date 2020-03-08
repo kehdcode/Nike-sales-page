a273253375d8af722a7436a1569508f4451c894f
kehdcode <kehindedawood@gmail.com>
2020-03-08 13:43:43 -0700

    created the cart feature using vanilla js

--- main.js
+++ main.js
@@ -1,12 +1,53 @@
 
 
+//Function to get form input
+calQuantity = (event) => {
+    let price = 199.00;
+    let vat = 7.5;
+    let quantity = document.getElementById("quantity");
+    let quantityValue = quantity.value;
+    console.log(quantityValue);
+    
+/*if (quantityValue === 0 || quantityValue == " "){
+    event.preventDefault();
+    console.log("Pleaase enter a number of shoes that you want");
+    
+}*/
+//Function to display quantity of product in the cart
+displayQty = ()=>{
+    let cart = document.getElementById('cart');
+    cart.innerText = quantityValue;
+    console.log(cart)
+}
+displayQty();
+
+//Function to calculate Total price
+totalPrice = ()=>{
+    priceSum = price*quantityValue;
+    console.log(priceSum);
+    
+    let plusVat = 7.5/100 * priceSum;
+    console.log(plusVat);
+//Function to calculate vat 
+    vatIncl=() =>{
+        totalCost = priceSum+plusVat;
+        console.log(totalCost);
+    }
+}
+    totalPrice();
+    vatIncl();
+    
+}
+
+
+
+
+//Function to change shoe color when hover a color
 function blueIn(x) {
-    console.log('Hello world');
     x.style.transform = "scale(1.5)";
     document.getElementById("myImage").src ="Images/shoe.png"
 }
 function blueOut(x) {
-    console.log('Hello world');
     x.style.transform = "scale(1)";
     document.getElementById("myImage").src ="Images/shoe.png"
 }
@@ -16,10 +57,9 @@ function redIn(x) {
     console.log('Hello world');
     x.style.transform = "scale(1.5)";
     document.getElementById("myImage").src ="Images/Redshoe.png"
-    y.style.backgroundGradient = "red";
+    
 }
 function redOut(x) {
-    console.log('Hello world');
     x.style.transform = "scale(1)";
     document.getElementById("myImage").src ="Images/shoe.png"
 }
@@ -30,19 +70,16 @@ function purpleIn(x) {
     document.getElementById("myImage").src ="Images/purpleshoe.png"
 }
 function purpleOut(x) {
-    console.log('Hello world');
     x.style.transform = "scale(1)";
     document.getElementById("myImage").src ="Images/shoe.png"
 }
 
 function greenIn(x) {
-    console.log('Hello world');
     x.style.transform = "scale(1.5)";
     document.getElementById("myImage").src ="Images/greenshoe.png"
     
 }
 function greenOut(x) {
-    console.log('Hello world');
     x.style.transform = "scale(1)";
     document.getElementById("myImage").src ="Images/greenshoe.png"
 }
